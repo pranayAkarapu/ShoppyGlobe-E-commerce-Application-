05b26792103a363db1ed632a5cfdc176719557eb
pranayAkarapu <pranayakarapu@gmail.com>
2025-04-03 15:21:51 +0530

    Initial-Changes

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,7 +1,7 @@
 import useFetch from "../utils/useFetch"
-import { useParams } from "react-router-dom"
+import { Navigate, replace, useParams } from "react-router-dom"
 import "../CSS-components/productdetail.css"
-
+import { useNavigate } from "react-router-dom";
 
 /**
  * ProductDetail Component - Fetches and displays details of a specific product.
@@ -11,16 +11,19 @@ import "../CSS-components/productdetail.css"
 function ProductDetail(){
     // Get product ID from the URL
     const {id} = useParams();
+    const navigate = useNavigate();
     const {data: product, error, loading} = useFetch(`https://dummyjson.com/products/${id}`);
 
     // Show loading state while fetching data
     if(loading) return <p>Loading...</p>;
 
     // Show error message if fetching fails
-    if(error) return <p>Error...</p>;
+    if(error || !product || product.message){
+        navigate("/NotFound", {replace:true})
+        return null;
+    }
 
     // Handle case where no product is found
-    if(!product) return <p>No product found</p>;
 
     return(
         <div className="product-detail">
