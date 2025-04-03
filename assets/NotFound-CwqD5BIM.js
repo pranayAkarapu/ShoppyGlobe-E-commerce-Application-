ef9ea8d9476c9ead608ee1ce9e6cb79d27c9367e
pranayAkarapu <pranayakarapu@gmail.com>
2025-04-03 15:47:08 +0530

    Updates

--- assets/NotFound-CwqD5BIM.js
+++
@@ -1,1 +0,0 @@
-import{j as o,L as e}from"./index-Bus_n2v5.js";function t(){return o.jsxs(o.Fragment,{children:[o.jsx("h1",{children:"Oops!! Page Not Found"}),o.jsx("p",{children:"The page you are looking for does not exist."}),o.jsx(e,{to:"/",children:"Go to Home"})]})}export{t as default};
